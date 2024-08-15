80fd54b9eed3252ef6e848bd827fa2dbb9f2c8e8
Nishchay Bhutoria <bhutorianishchay@gmail.com>
2024-08-16 02:32:31 +0530

    added missing domains and features and fixed sampling frequency

--- task-1-generate-features.py
+++ task-1-generate-features.py
@@ -3,16 +3,11 @@ import pandas as pd
 import tsfel
 from pathlib import Path
 
-# Base directory path
 base_dir = 'Combined/Train'
-
-# Output directory for TSFEL features
 output_base_dir = 'TSFEL_3axes_allfeatures'
 
-# Activities to consider
 activities = ['LAYING', 'SITTING', 'STANDING', 'WALKING', 'WALKING_UPSTAIRS', 'WALKING_DOWNSTAIRS']
 
-# Load data for each activity and process each subject individually
 for activity in activities:
     activity_dir = os.path.join(base_dir, activity)
     output_activity_dir = os.path.join(output_base_dir, activity)
@@ -23,10 +18,16 @@ for activity in activities:
     for file in subject_files:
         file_path = os.path.join(activity_dir, file)
         df = pd.read_csv(file_path)
+        # print(df)
         
         # Apply TSFEL feature extraction
         cfg = tsfel.get_features_by_domain()  # Adjust this based on the domains you need
-        features = tsfel.time_series_features_extractor(cfg, df, freq=50)
+        # print(cfg)
+        for domain in cfg:
+            for feature in cfg[domain]:
+                cfg[domain][feature]['use'] = 'yes' # use all features, even ones disabled by default
+
+        features = tsfel.time_series_features_extractor(cfg, df, fs=50)
         
         # Construct the output filename
         subject_id = file.split('.')[0]  # Assuming file naming convention allows this
