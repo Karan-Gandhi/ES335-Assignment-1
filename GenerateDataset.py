15bd286c0af89ae88aeca58e17444fb337e8afc5
Nishchay Bhutoria <bhutorianishchay@gmail.com>
2024-08-25 22:44:04 +0530

    removed redundant pca plot

--- GenerateDataset.py
+++ GenerateDataset.py
@@ -2,24 +2,23 @@ import os
 import numpy as np
 import pandas as pd
 
-filepath = os.path.join(os.path.abspath('..'), 'Datasets', 'UCI HAR Dataset')
-f = open(os.path.join(filepath, 'features.txt'), 'r')
-lines = f.readlines()
-f.close()
-features = []
-exists = {}
+def generate_dataset(filepath: str = os.path.join('Datasets', 'UCI HAR Dataset'), folder: str = 'Train') -> pd.DataFrame:
+    f = open(os.path.join(filepath, 'features.txt'), 'r')
+    lines = f.readlines()
+    f.close()
+    features = []
+    exists = {}
 
-for line in lines:
-    new_line = line.replace('\n', '').replace('-', '_')[line.find(' ') + 1:]
-    if new_line in exists:
-        # for features like fBodyAcc_bandsEnergy()_1,8 with multiple occurrences
-        features.append(new_line + "_" + str(exists[new_line]))
-        exists[new_line] += 1
-    else:
-        features.append(new_line)
-        exists[new_line] = 1
+    for line in lines:
+        new_line = line.replace('\n', '').replace('-', '_')[line.find(' ') + 1:]
+        if new_line in exists:
+            # for features like fBodyAcc_bandsEnergy()_1,8 with multiple occurrences
+            features.append(new_line + "_" + str(exists[new_line]))
+            exists[new_line] += 1
+        else:
+            features.append(new_line)
+            exists[new_line] = 1
 
-def generate_dataset(folder: str = 'Train') -> pd.DataFrame:
     dataset_path = os.path.join(filepath, folder)
     X_file = open(os.path.join(dataset_path, f'X_{folder}.txt'))
     f.close()
