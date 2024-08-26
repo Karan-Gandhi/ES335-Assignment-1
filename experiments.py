d038ef7320c4e94a3ae79a7b4872b039e051956d
Karan Gandhi <karangandhi.programming@gmail.com>
2024-08-26 21:49:38 +0530

    Added a value for M

--- experiments.py
+++ experiments.py
@@ -14,8 +14,8 @@ num_average_time = 100  # Number of times to run each experiment to calculate th
 
 # Function to create fake data (take inspiration from usage.py)
 # Real input real output
-N_max = [1, 5, 10, 30, 60]
-M_max = [1, 5, 10, 15, 20]
+N_values = [1, 5, 10, 30, 60]
+M_values = [1, 5, 10, 15, 20]
 
 def get_data(type, N, M):
     np.random.seed(42)
@@ -106,10 +106,10 @@ def plot_graph(N_vals, M_vals, fn, type):
         
     plot_twin_axis_graph(M_vals, training_times, testing_times, type + " wrt M", 'M')
     
-plot_graph(N_max, M_max, get_time_real_input_real_output, "real_input_real_output")
-plot_graph(N_max, M_max, get_time_real_input_real_output, "real_input_discrete_output")
-plot_graph(N_max, M_max, get_time_real_input_real_output, "discrete_input_real_output")
-plot_graph(N_max, M_max, get_time_real_input_real_output, "discrete_input_discrete_output")
+plot_graph(N_values, M_values, get_time_real_input_real_output, "real_input_real_output")
+plot_graph(N_values, M_values, get_time_real_input_real_output, "real_input_discrete_output")
+plot_graph(N_values, M_values, get_time_real_input_real_output, "discrete_input_real_output")
+plot_graph(N_values, M_values, get_time_real_input_real_output, "discrete_input_discrete_output")
 # ...
 # Function to calculate average time (and std) taken by fit() and predict() for different N and P for 4 different cases of DTs
 # ...
